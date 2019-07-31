3a4010eed05f349e7265fe8f82116e1c3c8b8f49
obp3ter <15703843+obp3ter@users.noreply.github.com>
2019-07-31 10:54:02 +0300

    comm

---
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/dto/UserDTOEntityMapper.java
@@ -0,0 +1,42 @@
+package ro.msg.edu.jbugs.dto;
+
+import ro.msg.edu.jbugs.model.entity.User;
+
+import javax.ejb.Singleton;
+
+@Singleton
+public class UserDTOEntityMapper extends BaseDTOEntityMapper<User,UserDTO> {
+
+    @Override
+    public User getEntityFromDTO(UserDTO userDTO)
+    {
+        User user = new  User();
+        //user.setID(userDTO.getID());
+        user.setCounter(userDTO.getCounter());
+        user.setEmail(userDTO.getEmail());
+        user.setFirstName(userDTO.getFirstName());
+        user.setLastName(userDTO.getLastName());
+        user.setMobileNumber(userDTO.getMobileNumber());
+        user.setPassword(userDTO.getPassword());
+        user.setUsername(userDTO.getUsername());
+        user.setStatus(userDTO.getStatus());
+        return user;
+    }
+
+    @Override
+    public UserDTO getDTOFromEntity(User user)
+    {
+        UserDTO userDTO = new  UserDTO();
+        userDTO.setID(user.getID());
+        userDTO.setCounter(user.getCounter());
+        userDTO.setEmail(user.getEmail());
+        userDTO.setFirstName(user.getFirstName());
+        userDTO.setLastName(user.getLastName());
+        userDTO.setMobileNumber(user.getMobileNumber());
+        userDTO.setPassword(user.getPassword());
+        userDTO.setUsername(user.getUsername());
+        userDTO.setStatus(user.getStatus());
+        return userDTO;
+    }
+
+}
---
+++ jbugs-client/src/main/java/servlet/TestServlet.java
@@ -0,0 +1,61 @@
+package servlet;
+
+import javafx.util.Pair;
+import ro.msg.edu.jbugs.bug.BugManager;
+import ro.msg.edu.jbugs.dto.BugDTO;
+import ro.msg.edu.jbugs.dto.UserDTO;
+import ro.msg.edu.jbugs.user.UserManager;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet("/test")
+public class TestServlet extends HttpServlet {
+
+    private String message;
+    private UserDTO userDTO = new UserDTO();
+    @EJB
+    private UserManager userManager;
+    @EJB
+    private BugManager bugManager;
+
+    public void init() throws ServletException {
+        // Do required initialization
+        message = "Hello World";
+        userDTO.setCounter(0);
+        userDTO.setEmail("teste");
+        userDTO.setFirstName("testfn");
+        userDTO.setLastName("testln");
+        userDTO.setMobileNumber("testmn");
+        userDTO.setPassword("testp");
+        userDTO.setUsername("testun");
+        userDTO.setStatus(1);
+
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // Set response content type
+        response.setContentType("text/html");
+        // Actual logic goes here.
+        PrintWriter out = response.getWriter();
+        out.println("<h1>" + message + "</h1>");
+        Pair<UserDTO, List<BugDTO>> createdBugs = userManager.getCreatedBugs(1);
+        List<Pair<UserDTO, List<BugDTO>>> createdBugsForAll = userManager.getCreatedBugsForAll();
+        createdBugsForAll.forEach(s->out.println("<p> User " + s.getKey().getFirstName() + " " + s.getKey().getLastName() + " created " + s.getValue().size() + " bugs.</p>"));
+        userManager.getUAB();
+
+    }
+
+    public void destroy() {
+        // do nothing.
+    }
+}
