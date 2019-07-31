1bb75e5c7ffac93163a50ffaebbaac4ee2da6e87
Peter Oprea-Benko <15703843+obp3ter@users.noreply.github.com>
2019-07-31 10:29:30 +0300

    Revert "Init2"
    This reverts commit eced26c19e4f80aabfab8a05e0ebd17da54ffa57.

--- jbugs-business/src/main/java/ro/msg/edu/jbugs/user/UserManager.java
+++ jbugs-business/src/main/java/ro/msg/edu/jbugs/user/UserManager.java
@@ -1,9 +1,6 @@
 package ro.msg.edu.jbugs.user;
 
 
-import javafx.util.Pair;
-import ro.msg.edu.jbugs.dto.BugDTO;
-import ro.msg.edu.jbugs.dto.BugDTOEntityMapper;
 import ro.msg.edu.jbugs.dto.UserDTO;
 import ro.msg.edu.jbugs.dto.UserDTOEntityMapper;
 import ro.msg.edu.jbugs.model.dao.UserDao;
@@ -11,7 +8,6 @@ import ro.msg.edu.jbugs.model.entity.User;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -23,9 +19,6 @@ public class UserManager {
 
     @EJB
     private UserDTOEntityMapper userMapper;
-
-    @EJB
-    private BugDTOEntityMapper bugMapper;
 
     public void insertUser(UserDTO userDTO)
     {
@@ -40,27 +33,6 @@ public class UserManager {
     {
         List<User> userList = userDao.findALL();
         return userMapper.getDTOsFromEntities(userList);
-    }
-
-    public Pair<UserDTO,List<BugDTO>> getCreatedBugs(Integer Id)
-    {
-       User user = userDao.findUser(Id);
-
-       return new Pair<>(userMapper.getDTOFromEntity(user),bugMapper.getDTOsFromEntities(user.getCreatedBugs()));
-    }
-    public List<Pair<UserDTO,List<BugDTO>>> getCreatedBugsForAll()
-    {
-        List<Pair<UserDTO,List<BugDTO>>> resultList = new ArrayList<>();
-        userDao.findALL().forEach(s->
-                {
-                    resultList.add(new Pair<>(userMapper.getDTOFromEntity(s),bugMapper.getDTOsFromEntities(s.getCreatedBugs())));
-                }
-        );
-        return resultList;
-    }
-    public void getUAB()
-    {
-        userDao.getUAB();
     }
 
 }
--- jbugs-client/src/main/java/servlet/TestServlet.java
+++ jbugs-client/src/main/java/servlet/TestServlet.java
@@ -1,6 +1,5 @@
 package servlet;
 
-import javafx.util.Pair;
 import ro.msg.edu.jbugs.bug.BugManager;
 import ro.msg.edu.jbugs.dto.BugDTO;
 import ro.msg.edu.jbugs.dto.UserDTO;
@@ -48,11 +47,8 @@ public class TestServlet extends HttpServlet {
         // Actual logic goes here.
         PrintWriter out = response.getWriter();
         out.println("<h1>" + message + "</h1>");
-        Pair<UserDTO, List<BugDTO>> createdBugs = userManager.getCreatedBugs(1);
-        List<Pair<UserDTO, List<BugDTO>>> createdBugsForAll = userManager.getCreatedBugsForAll();
-        createdBugsForAll.forEach(s->out.println("<p> User " + s.getKey().getFirstName() + " " + s.getKey().getLastName() + " created " + s.getValue().size() + " bugs.</p>"));
-        userManager.getUAB();
-
+        List<BugDTO> bugList = bugManager.getByAssignedId(1);
+        bugList.forEach(s -> out.println(s.toString()));
     }
 
     public void destroy() {
